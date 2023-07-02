32882c2401bf7c60045a442e6cf88fc171811664
Vitor L <vitorlopesfranchi@gmail.com>
2023-07-02 13:49:16 -0300

    add basic getall

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemsService } from './services/items.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(
+    private itemsService: ItemsService
+  ) { }
+
+  ngOnInit() {
+    this.itemsService.getAllItems()
+      .subscribe(data => {
+        console.log(data)
+      })
+  }
 }
--- client/src/app/services/items.service.ts
+++ client/src/app/services/items.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private baseURL = `http://localhost:3000/items`
+
+  getAllItems(): Observable<any> {
+    return this.http.get(`${this.baseURL}/`)
+ }
 }
